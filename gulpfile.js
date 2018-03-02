0617003e7a96bce06d8b7c13173b1baeb97d643d
Jose Pinto <ejosepintog@gmail.com>
2018-03-02 18:42:13 -0300

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp'),
+    clean = require('gulp-clean'),
+    zip = require('gulp-zip');
+
+gulp.task('clean', function () {
+    return gulp.src('dist')
+        .pipe(clean({ force: true }));
+});
+
+gulp.task('zip', function () {
+    return gulp.src('dist/**/*')
+        .pipe(zip('site.zip'))
+        .pipe(gulp.dest('.'));
+});
